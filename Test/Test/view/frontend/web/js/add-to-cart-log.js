ea4173ba4cbbce4b1a39e9e679eab7d28adb2142
nhattdvp211202 <162154530+nhattdvp211202@users.noreply.github.com>
2024-10-04 14:53:32 +0700

    Delete Test/Test directory

--- Test/Test/view/frontend/web/js/add-to-cart-log.js
+++
@@ -1,9 +0,0 @@
-define(['jquery'], function ($) {
-    'use strict';
-
-    return function (config) {
-        $(document).on('click', '.action.tocart', function (event) {
-            console.log(config.message);
-        });
-    };
-});
