dce636dc0a023ce423f2f055ff4f7ecc1beb9e63
nhattdvp211202 <162154530+nhattdvp211202@users.noreply.github.com>
2024-10-04 14:53:11 +0700

    Delete Test/MagentoJS directory

--- Test/MagentoJS/registration.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-use \Magento\Framework\Component\ComponentRegistrar;
-
-ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Tigren_MagentoJS', __DIR__);
--- Test/MagentoJS/view/frontend/requirejs-config.js
+++
@@ -1,8 +0,0 @@
-// app/code/Vendor/ModuleName/view/frontend/requirejs-config.js
-var config = {
-    map: {
-        '*': {
-            'magentojs': 'Tigren_MagentoJS/js/example'
-        }
-    }
-};
